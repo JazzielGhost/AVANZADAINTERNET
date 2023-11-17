56f64230dcc5e50099538652630d7d8710b27c58
JazzielGod <jazzielbh@gmail.com>
2023-11-16 21:45:25 -0700

    update - ejercicios realizados para el maquetador

---
+++ PROJECT-LARAVEL/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('container')
+    <h1>Hola q tal home</h1>
+@endsection
--- PROJECT-LARAVEL/resources/views/layouts/app.blade.php
+++ PROJECT-LARAVEL/resources/views/layouts/app.blade.php
@@ -1,9 +1,59 @@
-@extends('name') //extender todo la cascara de otro archivo
+<!DOCTYPE html>
 
-@included('name') //incluye una parte de otro archivo
+<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-theme="theme-default" data-assets-path="../../assets/" data-template="vertical-menu-template">
 
-@section('container')
-        //aqui iria el codigo
-@endsection
+@include('layouts.header')
 
-@yield('container') //ira en el archivo de app
+<body>
+
+  
+  <!-- ?PROD Only: Google Tag Manager (noscript) (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DDHKGP" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  
+  <!-- Layout wrapper -->
+<div class="layout-wrapper layout-content-navbar  ">
+  <div class="layout-container">
+<!-- Menu sidebar -->
+
+@include('layouts.sidebar')
+<!-- / Menu -->
+    <!-- Layout container -->
+    <div class="layout-page">
+
+<!-- Navbar -->
+
+@include('layouts.navbar')
+
+<!-- / Navbar -->
+
+    
+      <!-- Content wrapper -->
+      <div class="content-wrapper">
+
+        <!-- Content -->
+        
+          <div class="container-xxl flex-grow-1 container-p-y">
+            
+          
+<div class="row">
+  @yield('container')
+</div>
+
+</div>
+<!-- / Content -->
+
+<!-- Footer -->
+@include('layouts.footer')
+<!-- / Footer -->
+
+<!-- Scripts -->
+@include('layouts.scripts')
+<!-- Scripts -->
+  
+</body>
+
+</html>
+
+<!-- beautify ignore:end -->
+
